8805fde9954af5bf7aa0fdebd3991fc6a3e60c2d
Abid Ali <studytimee@gmail.com>
2022-05-12 17:34:52 +0100

    Individual Test: Authentication Test : remove lowercase condition
    Removed lower case condition from the email validation check

--- src/accounts/validators/index.js
+++ src/accounts/validators/index.js
@@ -2,7 +2,8 @@
 import Joi from 'joi';
 
 const accountSchema = Joi.object({
-    email: Joi.string().email().lowercase().required(),
+    //email: Joi.string().email().lowercase().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(7).required().regex(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{7,20}$/),
     firstName: Joi.string().alphanum().min(2).max(30),
     lastName: Joi.string().alphanum().min(2).max(30)
